0bb99d95cbd6f6a7bf2de60f7f89f93df0b58b06
Khlopkov Dmitry <krkrjtjt999@gmail.com>
2021-07-13 12:46:13 +0300

    Initial project

---
+++ Backend/src/main/kotlin/ru/ifmo/software_engineering/afterlife/AfterlifeApplication.kt
@@ -0,0 +1,11 @@
+package ru.ifmo.software_engineering.afterlife
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AfterlifeApplication
+
+fun main(args: Array<String>) {
+	runApplication<AfterlifeApplication>(*args)
+}
---
+++ Backend/src/test/kotlin/ru/ifmo/software_engineering/afterlife/AfterlifeApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.ifmo.software_engineering.afterlife
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AfterlifeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
