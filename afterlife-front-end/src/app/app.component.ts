3d7ac1940150ce3a215c152deb042e3fe241dbaa
Boris-Chang <cjynoodles@gmail.com>
2021-07-24 10:52:32 +0800

    first commit

---
+++ afterlife-front-end/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, VERSION } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Afterlife 2.0';
+
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 2000;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+  }
+}
---
+++ afterlife-front-end/src/app/components/banner-bar/banner-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-banner-bar',
+  templateUrl: './banner-bar.component.html',
+  styleUrls: ['./banner-bar.component.scss'],
+})
+export class BannerBarComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() : void{ }
+
+}
