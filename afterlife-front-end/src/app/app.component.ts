44087f649fe67b05c6b4b6855919cbf940578426
Boris-Chang <cjynoodles@gmail.com>
2021-08-09 16:31:45 +0800

    updata table and quality-list

--- afterlife-front-end/src/app/app.component.ts
+++ afterlife-front-end/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, VERSION } from '@angular/core';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +8,6 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 export class AppComponent {
   title = 'Afterlife 2.0';
 
-  constructor(config: NgbCarouselConfig) {
-    // customize default values of carousels used by this component tree
-    config.interval = 2000;
-    config.keyboard = true;
-    config.pauseOnHover = true;
+  constructor() {
   }
 }
