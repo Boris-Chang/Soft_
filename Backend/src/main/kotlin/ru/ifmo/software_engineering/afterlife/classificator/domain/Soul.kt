ad9b02e3e1feab0187123a740ad75c3141a7e50b
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2021-08-28 18:13:38 +0300

    Hide Soul ID from request

--- Backend/src/main/kotlin/ru/ifmo/software_engineering/afterlife/classificator/domain/Soul.kt
+++ Backend/src/main/kotlin/ru/ifmo/software_engineering/afterlife/classificator/domain/Soul.kt
@@ -1,8 +1,12 @@
 package ru.ifmo.software_engineering.afterlife.classificator.domain
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
 import java.util.*
 
 data class Soul(
+    @Schema(readOnly = true)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     val id: Long,
     val firstName: String,
     val lastName: String,
