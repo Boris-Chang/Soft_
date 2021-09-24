a752d59e5d5e0604defda30779c91e090eb8b6e9
dmitriy.khlopkov <dmitriy.khlopkov@arcadia.spb.ru>
2021-09-24 13:57:44 +0300

    CORS settings added

---
+++ Backend/src/main/kotlin/ru/ifmo/software_engineering/afterlife/config/CorsConfig.kt
@@ -0,0 +1,16 @@
+package ru.ifmo.software_engineering.afterlife.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("*")
+    }
+}
