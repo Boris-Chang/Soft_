0bc2355a7a4135d5054eca812376ac539d7fce7f
Boris-Chang <cjynoodles@gmail.com>
2021-12-30 00:36:41 +0800

    add function - Login

--- afterlife-front-end/src/app/modules/survey/survey.component.ts
+++ afterlife-front-end/src/app/modules/survey/survey.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SurveyComponent implements OnInit {
 
+  sideBarOpen = true;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  sideBarToggler() {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
 }
