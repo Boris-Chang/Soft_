d81ace34ea8de42ef55ea227573c9f2f9a4e39ab
dmitrii.khlopkov <dmitrii.khlopkov@semrush.com>
2022-01-19 16:58:28 +0300

    Added validation that user has correct role before getting soul

--- Backend/src/main/kotlin/ru/ifmo/software_engineering/afterlife/users/constants/RoleNames.kt
+++ Backend/src/main/kotlin/ru/ifmo/software_engineering/afterlife/users/constants/RoleNames.kt
@@ -2,6 +2,8 @@ package ru.ifmo.software_engineering.afterlife.users.constants
 
 object RoleNames {
     const val ADMIN: String = "ADMIN"
+    const val GOD: String = "GOD"
+    const val DEVIL: String = "DEVIL"
     const val HEAVEN_ADVOCATE: String = "HEAVEN_ADVOCATE"
     const val HEAVEN_PROSECUTOR: String = "HEAVEN_PROSECUTOR"
 }
