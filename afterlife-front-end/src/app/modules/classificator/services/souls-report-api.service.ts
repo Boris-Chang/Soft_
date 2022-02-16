f2b31479e9fa4dc24c1607b5df47cf108220a7a9
Changjiyuan <61522364+Boris-Chang@users.noreply.github.com>
2022-02-16 10:17:00 +0800

    Update souls-report-api.service.ts

--- afterlife-front-end/src/app/modules/classificator/services/souls-report-api.service.ts
+++ afterlife-front-end/src/app/modules/classificator/services/souls-report-api.service.ts
@@ -9,7 +9,8 @@ import { PagedResult } from "../../../shared/models";
 export class SoulsReportApiService {
   //TODO: move this to config, register as a service and inject it through constructor via Dependency Injection
   //https://stackoverflow.com/questions/43193049/app-settings-the-angular-way/43193574#43193574
-  private readonly apiUrl = 'http://localhost:8080/api'
+  private readonly apiUrl = 'http://localhost:8080/api';
+  private baseUrl = 'http://localhost:8080';
 
   constructor(private readonly http: HttpClient) {
   }
@@ -20,6 +21,26 @@ export class SoulsReportApiService {
 
   public setGoodnessReport(soulId: number,csvGoodnessReport: FormData) : Observable<any>{
     return this.http.put<any>(`${this.apiUrl}/souls/${soulId}/reports/goodness`,csvGoodnessReport);
+  }
+  
+  uploadSinsReport(soulId: number, csvSinsReport: File): Observable<HttpEvent<any>> {
+    const formData: FormData = new FormData();
+    formData.append('file', csvSinsReport);
+    const req = new HttpRequest('PUT', `${this.baseUrl}/souls/${soulId}/reports/sins`, formData, {
+      reportProgress: true,
+      responseType: 'json'
+    });
+    return this.http.request(req);
+  }
+
+  uploadGoodnessReport(soulId: number, csvGoodnessReport: File): Observable<HttpEvent<any>> {
+    const formData: FormData = new FormData();
+    formData.append('file', csvGoodnessReport);
+    const req = new HttpRequest('PUT', `${this.baseUrl}/souls/${soulId}/reports/goodness`, formData, {
+      reportProgress: true,
+      responseType: 'json'
+    });
+    return this.http.request(req);
   }
 
 }
