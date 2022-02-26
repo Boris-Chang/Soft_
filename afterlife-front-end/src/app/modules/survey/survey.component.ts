accfb03d94def14c71fc858b90c173f4fbc2e7b8
Changjiyuan <61522364+Boris-Chang@users.noreply.github.com>
2022-02-27 05:11:32 +0800

    Update survey.component.ts

--- afterlife-front-end/src/app/modules/survey/survey.component.ts
+++ afterlife-front-end/src/app/modules/survey/survey.component.ts
@@ -1,60 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-import {DataSource} from '@angular/cdk/collections';
-import {Observable, ReplaySubject} from 'rxjs';
+import { Subscription } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: string;
-  symbol: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Опрос качества', weight: 'googlesheet', symbol: 'Пыточника'},
-  {position: 2, name: 'Опрос качества', weight: 'googlesheet', symbol: 'Грешника'},
-  {position: 3, name: 'Опрос качества', weight: 'googlesheet', symbol: 'Пыточника'},
-  {position: 4, name: 'Опрос качества', weight: 'googlesheet', symbol: 'Грешника'}
-];
-  /**
-  * @title Adding and removing data when using an observable-based datasource.
-  */
+import { SurveyApiService } from './service/survey-api.service';
+
+import { Result } from './model/result.model';
+import { SurveyInfo } from './model/surveyInfo.model';
+import { DialogComponent } from './dialog/dialog.component';
+
+const Surveys = 'Surveys';
+
 @Component({
   selector: 'app-survey',
   templateUrl: './survey.component.html',
   styleUrls: ['./survey.component.scss']
 })
 export class SurveyComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataToDisplay = [...ELEMENT_DATA];
-  dataSource = new ExampleDataSource(this.dataToDisplay);
-  addData() {
-  const randomElementIndex = Math.floor(Math.random() * ELEMENT_DATA.length);
-  this.dataToDisplay = [...this.dataToDisplay, ELEMENT_DATA[randomElementIndex]];
-  this.dataSource.setData(this.dataToDisplay);
+
+  subscription: Subscription;
+  constructor(private surveyApi: SurveyApiService, public dialog: MatDialog) { }
+ 
+  openDialog() {
+    this.dialog.open(DialogComponent);    
   }
-  removeData() {
-  this.dataToDisplay = this.dataToDisplay.slice(0, -1);
-  this.dataSource.setData(this.dataToDisplay);
-  }
-  constructor() { }
+  
+
+  resultPage: Result;
+  surveys: SurveyInfo[];
+  survey: string;
 
   ngOnInit(): void {
+    this.subscription = this.surveyApi.getResultPageSurveys()
+    .subscribe( data => {
+      this.resultPage = data;
+      console.log(this.resultPage);
+      this.surveys = this.resultPage.results;
+      window.sessionStorage.setItem(Surveys, JSON.stringify(this.surveys));
+      console.log(this.surveys);
+    })
+    this.survey = window.sessionStorage.getItem(Surveys);
+    this.surveys = JSON.parse(this.survey);
+    // console.log(this.surveys);
   }
   sideBarOpen = true;
   sideBarToggler() {
     this.sideBarOpen = !this.sideBarOpen;
-  }
-}
-class ExampleDataSource extends DataSource<PeriodicElement> {
-  private _dataStream = new ReplaySubject<PeriodicElement[]>();
-  constructor(initialData: PeriodicElement[]) {
-  super();
-  this.setData(initialData);
-  }
-  connect(): Observable<PeriodicElement[]> {
-  return this._dataStream;
-  }
-  disconnect() {}
-  setData(data: PeriodicElement[]) {
-  this._dataStream.next(data);
   }
 }
